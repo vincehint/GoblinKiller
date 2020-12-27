cbbe08e635f48d664e1079a66f0049e117537450
Vince Hintermeister <vv.hintermeister@gmail.com>
2020-12-27 14:39:46 -0600

    The goblins move
    '

--- script.js
+++ script.js
@@ -3,38 +3,64 @@ console.log("we good bro")
 var ctx = game.getContext('2d')
 let movementDisplay = movement
 var movement = 10
-var image = document.getElementById("archer")
-var image2 = document.getElementById("goblin")
-ctx.drawImage(image, 10, 10,)
+//var image = document.getElementById("archer")
+var image = new Image()
+image.src = "https://webstockreview.net/images/archer-clipart-archery-2.png"
+//var image2 = document.getElementById("goblin")
+var image2 = new Image()
+image2.src = "https://i.dlpng.com/static/png/6354616_preview.png"
+//var arrow = document.getElementById("arrow")
 
 
-function Hero(image, x, y) {
+function Hero(image, x, y, ) {
     this.image = image
     this.x = x
     this.y = y
     this.render = function() {
-        ctx.drawImage(this.image, this.x, this.y)
+        ctx.drawImage(this.image, this.x, this.y, 50, 50)
     }
 }
 
-let hero = new Hero(image, 100, 200,)
 
-function Goblin(image2, x, y,) {
-    this.image = image2
+let hero = new Hero(image, 100, 200)
+
+function Goblin(image, x, y) {
+    this.image = image
     this.x = x
     this.y = y
+    this.update = function() {
+        if (hero.y<this.y){
+            this.y-=1
+        }
+        if (hero.y>this.y){
+            this.y+=1
+        }
+        if (hero.x<this.x){
+            this.x-=1
+        }
+        if (hero.x>this.x){
+            this.x+=1
+        }
+    }
     this.render = function() {
-        ctx.drawImage(this.image, this.x, this.y)
+        ctx.drawImage(this.image, this.x, this.y, 40, 40)
     }
 }
 
-let goblin = new Goblin(image2, 350, 100,)
+let goblins = []
 
+for (let i = 0; i<5; i++) {
+    goblins.push(new Goblin(image2, Math.random()*800, Math.random()*800))
+}
+console.log(goblins)
 
 let gameLoop = setInterval(() => {
-
     ctx.clearRect(0, 0, game.width, game.height)
     hero.render()
+    for (let i =0; i<goblins.length; i++) {
+        goblins[i].update()
+        goblins[i].render()
+    }
 },100)
 
 function movementHero(e) {
@@ -55,7 +81,7 @@ switch(e.key) {
 }
 document.addEventListener("keydown", movementHero)
 
-var arrow = document.getElementById("arrow")
+
 
 
 
