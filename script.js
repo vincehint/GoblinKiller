7e0b82d9c2d2fa63846f80481bde59a5758f82ef
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-01-04 21:16:43 -0600

    working so far

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ arrowLeft.src = "images/ArrowLeft.png"
 var arrowRight = new Image()
 arrowRight.src = "images/ArrowRight.png"
 let arrow = []
+var gameover = true
+
+
 
 function Hero(image, x, y, width, height) {
     this.image = image
@@ -28,12 +31,12 @@ function Hero(image, x, y, width, height) {
     }
 }
 
-function Goblin(image, x, y, width, height) {
+function Goblin(image, x, y, gwidth, gheight) {
     this.image = image
     this.x = x
     this.y = y
-    this.width = width
-    this.height = height
+    this.width = gwidth
+    this.height = gheight
     this.alive = true
     this.update = function() {
         if (hero.y<this.y){
@@ -86,9 +89,11 @@ function Arrow(image, x, y, width, height, direction) {
     }
 }
 
+
+
 function looseArrows() {
     //console.log("Thup")
-    arrow.push(new Arrow (arrowUp, hero.x, hero.y, 100, 100, hero.direction))
+    arrow.push(new Arrow (arrowUp, hero.x, hero.y, 40, 40, hero.direction))
     //console.log(arrow)
 }
 
@@ -97,19 +102,17 @@ let goblin = new Goblin
 let goblins = []
 
 for (let i = 0; i<5; i++) {
-    goblins.push(new Goblin(image2, Math.random()*800, Math.random()*800, 80, 80))
+    goblins.push(new Goblin(image2, Math.random()*800, Math.random()*800, 100, 100))
 }
 
 
-let gameLoop = setInterval(() => {                                                                                                                                                                                                                                                                                                              
+let gameLoop = setInterval(() => {
     ctx.clearRect(0, 0, game.width, game.height)
     hero.render()
-    //console.log(detectHit())
     for (let i =0; i<goblins.length; i++) {
         var collide = detectHit(hero, goblins[i])
         if (collide) {
             hero.alive = false
-            // console.log("Ouch!")
         }
         if (goblins[i].alive) {
             goblins[i].update()
@@ -120,8 +123,10 @@ let gameLoop = setInterval(() => {
             arrow[l].render()
             var shot = detectHit2(arrow[l], goblins[i])
             if (shot) {
+               
+               console.log(arrow[l].x, (arrow[l].x + arrow[l].width), arrow[l].y, (arrow[l].y + arrow[l].height))
+               console.log(goblins[i].x, (goblins[i].x + goblins[i].width), goblins[i].y, (goblins[i].y + goblins[i].height))
                goblins[i] = false
-            //console.log("biff")
         }
     }    
 }},100)
@@ -173,13 +178,12 @@ let detectHit = (hero, goblin) => {
     //console.log(arrow)
     //console.log(goblin)
     if (
-        goblin.x >= arrow.x  &&
+        goblin.x <= arrow.x + arrow.width &&
         arrow.x <= goblin.x + goblin.width &&
-        arrow.y <= goblin.y &&
+        goblin.y <= arrow.y + arrow.height &&
         arrow.y <= goblin.y + goblin.height
       ) {
         //console.log("ha!")
-        console.log("Hello")
         return true
       }
     return false 
